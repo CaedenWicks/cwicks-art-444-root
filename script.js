8008c22f317bb7b62b8a288f9ad163055c178c4c
CaedenWicks <cwicks7100@sdsu.edu>
2025-05-08 00:16:44 -0700

    Project 4

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function toggleMenu() {
   const menu = document.getElementById("mobileMenu");
   menu.classList.toggle("hidden");
 }
+document.querySelectorAll('#mobileMenu a').forEach(link => {
+  const targetId = link.getAttribute('data-target');
+  const icon = document.getElementById(targetId);
+
+  link.addEventListener('mouseenter', () => {
+    if (icon) icon.classList.add('visible');
+  });
+
+  link.addEventListener('mouseleave', () => {
+    if (icon) icon.classList.remove('visible');
+  });
+});
 
 document.querySelectorAll('#mobileMenu a').forEach(link => {
   link.addEventListener('click', () => {
@@ -12,4 +24,6 @@ document.querySelectorAll('#mobileMenu a').forEach(link => {
 // --- dark mode toggle comes AFTER ---
 document.getElementById('darkModeToggle').addEventListener('click', function() {
   document.body.classList.toggle('dark-mode');
+
+  
 });
